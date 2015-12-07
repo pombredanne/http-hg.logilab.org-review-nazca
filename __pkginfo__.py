3694492428e0b3e8728c82dece881e136cdbfab7
Denis Laxalde <denis.laxalde@logilab.fr>
2015-12-07 14:34:12 +0100

    [pkg] Fix copyright in __pkginfo__.py

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -1,21 +1,21 @@
 # pylint: disable-msg=W0622
-# copyright 2004-2012 LOGILAB S.A. (Paris, FRANCE), all rights reserved.
+# copyright 2004-2015 LOGILAB S.A. (Paris, FRANCE), all rights reserved.
 # contact http://www.logilab.fr/ -- mailto:contact@logilab.fr
 #
-# This file is part of rql.
+# This file is part of nazca.
 #
-# rql is free software: you can redistribute it and/or modify it under the
+# nazca is free software: you can redistribute it and/or modify it under the
 # terms of the GNU Lesser General Public License as published by the Free
 # Software Foundation, either version 2.1 of the License, or (at your option)
 # any later version.
 #
-# rql is distributed in the hope that it will be useful, but WITHOUT ANY
+# nazca is distributed in the hope that it will be useful, but WITHOUT ANY
 # WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
 # A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
 # details.
 #
 # You should have received a copy of the GNU Lesser General Public License along
-# with rql. If not, see <http://www.gnu.org/licenses/>.
+# with nazca. If not, see <http://www.gnu.org/licenses/>.
 """Nazca packaging information."""
 __docformat__ = "restructuredtext en"
 import sys
